1810daae2eeb8a66008d948703d61b781571f4f0
LeHien2818 <hienltv281823@gmail.com>
2025-06-12 03:07:48 +0700

    fix(Dockerfile): update build java version

--- app/src/main/java/call/monitor/app/config/KafkaConfiguration.java
+++ app/src/main/java/call/monitor/app/config/KafkaConfiguration.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -14,11 +15,21 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 @Configuration
 @EnableKafka
 public class KafkaConfiguration {
+    @Value("${kafka.broker.address}")
+    private String kafkaAddress;
 
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
+        String kafkaConnectionServer = "localhost:9092,localhost:9093";
+        if(kafkaAddress != null){
+            if(!kafkaAddress.equals(kafkaConnectionServer) &&
+                !kafkaAddress.equals("default")){
+                kafkaConnectionServer = kafkaAddress;
+            }
+        }
+
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092,localhost:9093");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConnectionServer);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "call-app-consumer");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, GsonDeserializer.class);
